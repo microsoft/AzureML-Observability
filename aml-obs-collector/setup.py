52dbdb1f6384f43da09e912c8f213fa79b2c782a
james-tn <james.nguyen@microsoft.com>
2022-05-13 11:25:40 -0700

    fix path in init

--- aml-obs-collector/setup.py
+++ aml-obs-collector/setup.py
@@ -5,7 +5,7 @@ with open("../README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='aml-obs-collector',
-    version='0.0.2',
+    version='0.0.3',
     author='James Nguyen; Nicole Serafino',
     author_email='janguy@microsoft.com;nserafino@microsoft.com',
     description='Testing installation of Package',
