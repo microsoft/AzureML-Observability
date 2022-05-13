ce850991391e8837ead449545ca9fb2d0799f216
james-tn <james.nguyen@microsoft.com>
2022-05-13 15:13:27 -0700

    add Python enabling capability, change azure-ml to azure-ai-aml, update collector's data type mapping

--- aml-obs-collector/setup.py
+++ aml-obs-collector/setup.py
@@ -5,7 +5,7 @@ with open("../README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='aml-obs-collector',
-    version='0.0.5',
+    version='0.0.6',
     author='James Nguyen; Nicole Serafino',
     author_email='janguy@microsoft.com;nserafino@microsoft.com',
     description='Testing installation of Package',
