2e8681a7ec40a6e4a60df65200a99d1273bfce04
james-tn <james.nguyen@microsoft.com>
2022-05-12 17:39:49 -0700

    add setup file

---
+++ aml-obs-collector/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+with open("../README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='aml-obs-collector',
+    version='0.0.5',
+    author='James Nguyen; Nicole Serafino',
+    author_email='janguy@microsoft.com;nserafino@microsoft.com',
+    description='Testing installation of Package',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/microsoft/AzureML-Observability',
+    project_urls = {
+        "Bug Tracker": "https://github.com/microsoft/AzureML-Observability/issues"
+    },
+    license='MIT',
+    packages=['monitoring'],
+    install_requires=['azure-identity>=1.7.0','azure-mgmt-kusto==2.2.0','azure-kusto-data==3.1.2','azure-kusto-ingest==3.1.2'],
+)
